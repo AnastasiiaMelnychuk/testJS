1b669fdeaae418f3b5bd93258ce201df2184a8c0
Nasitia <Nasitia@Tarass-MacBook-Air.local>
2020-04-04 00:08:49 +0300

    renamed

--- lesson_1_task_2.js
+++ lesson_1_task_2.js
@@ -3,8 +3,8 @@ var sideTwo = parseFloat(prompt('Enter the length of the second side', 20));
 var angle = parseFloat(prompt('Enter the angel between two sides', 30));
 
 var sideThree = Math.sqrt(Math.pow(sideOne,2) + Math.pow(sideTwo,2) - 2 * sideOne * sideTwo * Math.cos(Math.PI / 180 * angle));
-var square = roundToTwo(sideOne * sideTwo * Math.sin(Math.PI / 180 * angle) / 2);
-var perimeter = roundToTwo(sideOne + sideTwo + sideThree);
+var square = sideOne * sideTwo * Math.sin(Math.PI / 180 * angle) / 2;
+var perimeter = sideOne + sideTwo + sideThree;
 
 if (sideOne > 0 && sideTwo > 0 && angle > 0) {
   console.log(`
